37859f998522a80d84e71b942c65373abf2e345d
adupuis <adupuis@genymobile.com>
2011-06-09 13:19:22 +0200

    - ajout d'une fonction retournant lea liste des jours ouvrées entre 2 dates warning(fonction getWorkedDays non testée)

--- classes/GenyTools.php
+++ classes/GenyTools.php
@@ -1,8 +1,7 @@
 <?php
 
 class GenyTools {
-	static function getYearHolidaysNG(){
-		date_default_timezone_set('Europe/Paris');
+	static function getYearHolidays(){
 		$year = (int)date('Y');
 		// Liste des jours feriés
 		$arr_bank_holidays[] = $year.'-1-1'; // Jour de l'an
@@ -19,6 +18,19 @@ class GenyTools {
 		$arr_bank_holidays[] = date($year.'-n-j', $easter + (86400*39)); // Ascension
 		$arr_bank_holidays[] = date($year.'-n-j', $easter + (86400*50)); // Pentecote
 		return $arr_bank_holidays;
+	}
+	// Cette fonction prend deux dates en arguments sous forme de timestamp UNIX.
+	function getWorkedDaysList($start_date,$end_date){
+		$worked_days = array();
+		while ($date_start < $date_stop) {
+			// Si le jour suivant n'est ni un dimanche (0) ou un samedi (6), ni un jour férié, on incrémente les jours ouvrés
+			if (!in_array(date('w', $date_start), array(0, 6))
+			&& !in_array(date(date('Y', $date_start).'-n-j', $date_start), $arr_bank_holidays)) {
+				$worked_days[] = date('Y-m-j',$start_date);
+			}
+			$date_start = mktime(date('H', $date_start), date('i', $date_start), date('s', $date_start), date('m', $date_start), date('d', $date_start) + 1, date('Y', $date_start));
+		} 
+		return $worked_days;
 	}
 }
 
