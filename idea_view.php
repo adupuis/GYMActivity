0cf13f8463a41e537269ec26e8a43db166d5ad2c
ouroz <ouroz@gkar.(none)>
2011-06-25 19:27:40 +0200

    Formulaire d'ajout de commentaires dans la page de visualisation d'une idée.

--- idea_view.php
+++ idea_view.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set( 'error_reporting', E_ERROR );
+ini_set( 'display_errors', 1 );
+
 // Variable to configure global behaviour
 $header_title = 'GenY Mobile - Visualisation idée';
 $required_group_rights = 2;
@@ -16,6 +20,29 @@ $geny_profile = new GenyProfile();
 if( isset( $_POST['load_idea'] ) && $_POST['load_idea'] == "true" ) {
 	if( isset( $_POST['idea_id'] ) ) {
 		$geny_idea->loadIdeaById( $_POST['idea_id'] );
+	}
+	else {
+		$db_status .= "<li class=\"status_message_error\">Impossible de charger l'idée : id non spécifié.</li>\n";
+	}
+}
+else if( isset( $_POST['idea_message_create'] ) && $_POST['idea_message_create'] == "true" ) {
+	if( isset( $_POST['idea_message_idea_id'] ) ) {
+		$geny_idea->loadIdeaById( $_POST['idea_message_idea_id'] );
+		if( isset( $_POST['idea_message_content'] ) ) {
+
+			$logged_in_profile = new GenyProfile();
+			$logged_in_profile->loadProfileByUsername( $_SESSION['USERID'] );
+
+			if( $geny_idea_message->insertNewIdeaMessage( 'NULL', $_POST['idea_message_content'], $logged_in_profile->id, $_POST['idea_message_idea_id'] ) ) {
+				$db_status .= "<li class=\"status_message_success\">Commentaire ajouté avec succès.</li>\n";
+			}
+			else {
+				$db_status .= "<li class=\"status_message_error\">Erreur lors de l'ajout du commentaire.</li>\n";
+			}
+		}
+		else {
+			$db_status .= "<li class=\"status_message_error\">Certains champs obligatoires sont manquant. Merci de les remplir.</li>\n";
+		}
 	}
 	else {
 		$db_status .= "<li class=\"status_message_error\">Impossible de charger l'idée : id non spécifié.</li>\n";
@@ -96,7 +123,12 @@ if( isset( $_POST['load_idea'] ) && $_POST['load_idea'] == "true" ) {
 				<?php
 				foreach( $geny_profile->getAllProfiles() as $profile ) {
 					if( $geny_idea->submitter == $profile->id ) {
-						echo $profile->firstname." ".$profile->lastname;
+						if( $profile->firstname && $profile->lastname ) {
+							echo $profile->firstname." ".$profile->lastname;
+						}
+						else {
+							echo $profile->login;
+						}
 						break;
 					}
 				}
@@ -111,22 +143,27 @@ if( isset( $_POST['load_idea'] ) && $_POST['load_idea'] == "true" ) {
 
 		<br>
 		
-		<table cellspacing="0" cellpadding="4" class="messages_table">
+		<table cellspacing="0" cellpadding="4" class="idea_message_table">
 		<tbody>
 			<tr>
-			<th id="messages_table_header_author" class="messages_table_header">Auteurs</th>
-			<th id="messages_table_header_content" class="messages_table_header">Commentaires</th>
+			<th id="idea_message_table_header_author" class="idea_message_table_header">Auteurs</th>
+			<th id="idea_message_table_header_content" class="idea_message_table_header">Commentaires</th>
 			</tr>
 			<?php
 			$geny_idea_messages = $geny_idea_message->getIdeaMessagesListByIdeaId( $geny_idea->id );
 			foreach( $geny_idea_messages as $idea_message ) {
 				foreach( $geny_profile->getAllProfiles() as $profile ) {
 				if( $idea_message->profile_id == $profile->id ) {
-					$message_author = $profile->firstname." ".$profile->lastname;
+					if( $profile->firstname && $profile->lastname ) {
+						$message_author = $profile->firstname." ".$profile->lastname;
+					}
+					else {
+						$message_author = $profile->login;
+					}
 					break;
 				}
 				}
-				echo "<tr><td>".$message_author."</td><td>".$idea_message->content."</td></tr>";
+				echo "<tr id=\"idea_message_table_idea\"><td>".$message_author."</td><td>".$idea_message->content."</td></tr>";
 			}
 			if( $geny_idea->id < 0 ) {
 				$geny_idea->loadIdeaById( $ideas[0]->id );
@@ -134,6 +171,16 @@ if( isset( $_POST['load_idea'] ) && $_POST['load_idea'] == "true" ) {
 			?>
 		</tbody>
 		</table>
+
+		<br>
+
+		<form id="idea_message_create_form" action="idea_view.php" method="post">
+ 			<input type="hidden" name="idea_message_create" value="true" />
+ 			<input type="hidden" name="idea_message_idea_id" value="<?php echo $geny_idea->id ?>" />
+ 			<label for="idea_message_content">Poster un commentaire</label>
+			<textarea name="idea_message_content" id="idea_message_content" class="validate[required] text-input"></textarea>
+			<input type="submit" id="idea_message_create_form_submit" value="Poster" />
+		</form>
 	</p>
 </div>
 
