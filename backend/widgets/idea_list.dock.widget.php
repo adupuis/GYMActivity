438369da1cc2ba34a19a264dd8714047b9ba595e
adupuis <adupuis@genymobile.com>
2011-07-04 07:40:03 +0200

    Ajout du widget idea_list.dock.widget.php

---
+++ backend/widgets/idea_list.dock.widget.php
@@ -0,0 +1,6 @@
+<li class="idea_list">
+	<a href="idea_list.php">
+		<span class="dock_item_title">Liste des idées</span><br/>
+		<span class="dock_item_content">Lister toutes les idées de la boîte à idée Geny Mobile.</span>
+	</a>
+</li>
