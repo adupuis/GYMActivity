f185007330ea714f69f3ca6f48599c6081114555
adupuis <adupuis@genymobile.com>
2011-07-20 09:23:58 +0200

    Ajout de la gestion d'ACL sur la page d'admin qui n'affiche plus que le contenu auquel l'utilisateur à le droit d'accéder.

--- Web/home_admin.php
+++ Web/home_admin.php
@@ -16,16 +16,21 @@ include_once 'menu.php';
 <div id="maindock">
 	<ul>
 		<?php 
-			include 'backend/widgets/cra_validation_admin.dock.widget.php'; 
-			include 'backend/widgets/conges_validation.dock.widget.php'; 
-			include 'backend/widgets/profile_add.dock.widget.php'; 
-			include 'backend/widgets/profile_list.dock.widget.php'; 
-			include 'backend/widgets/client_add.dock.widget.php'; 
-			include 'backend/widgets/client_list.dock.widget.php'; 
-			include 'backend/widgets/task_add.dock.widget.php'; 
-			include 'backend/widgets/task_list.dock.widget.php'; 
-			include 'backend/widgets/project_add.dock.widget.php'; 
-			include 'backend/widgets/project_list.dock.widget.php'; 
+			include 'backend/widgets/user_admin_password_change.dock.widget.php';
+			if( $profile->rights_group_id == 1 ){
+				include 'backend/widgets/profile_add.dock.widget.php'; 
+				include 'backend/widgets/profile_list.dock.widget.php'; 
+			}
+			if( $profile->rights_group_id == 1 || $profile->rights_group_id == 2 ){
+				include 'backend/widgets/cra_validation_admin.dock.widget.php'; 
+				include 'backend/widgets/conges_validation.dock.widget.php'; 
+				include 'backend/widgets/client_add.dock.widget.php'; 
+				include 'backend/widgets/client_list.dock.widget.php'; 
+				include 'backend/widgets/task_add.dock.widget.php'; 
+				include 'backend/widgets/task_list.dock.widget.php'; 
+				include 'backend/widgets/project_add.dock.widget.php'; 
+				include 'backend/widgets/project_list.dock.widget.php';
+			}
 			include 'backend/widgets/idea_add.dock.widget.php'; 
 			include 'backend/widgets/idea_list.dock.widget.php'; 
 		?>
