ee25c1b9d0aae2489fab3491538675d48383259e
adupuis <adupuis@genymobile.com>
2011-07-20 08:54:28 +0200

    Ajout d'un widget menant à la page de modification du mot de passe.

---
+++ Web/backend/widgets/user_admin_password_change.dock.widget.php
@@ -0,0 +1,6 @@
+<li class="user_admin_password_change">
+	<a href="user_admin_password_change.php">
+		<span class="dock_item_title">Modif. password</span><br/>
+		<span class="dock_item_content">Modifier son mot de passe.</span>
+	</a>
+</li>
