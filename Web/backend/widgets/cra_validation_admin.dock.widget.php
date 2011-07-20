783e0c8565d835f7cdc88c4086c1259b38725531
adupuis <adupuis@genymobile.com>
2011-07-20 09:26:18 +0200

    Modification du text de description du widget de validation (manager) des cras pour ne plus le confondre avec la validation utilisateur.

--- Web/backend/widgets/cra_validation_admin.dock.widget.php
+++ Web/backend/widgets/cra_validation_admin.dock.widget.php
@@ -1,6 +1,8 @@
 <li class="cra_admin_generic">
 	<a href="cra_validation_admin.php">
 		<span class="dock_item_title">Validation CRA</span><br/>
-		<span class="dock_item_content">Validation des soumissions et des demandes de suppressions de CRAs.</span>
+		<span class="dock_item_content">
+			Validation des soumissions et des demandes de suppressions de CRAs. Cette validation ne peut être faîtes que par des managers.
+		</span>
 	</a>
 </li>
