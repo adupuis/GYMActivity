3e1236d9836aef2f58c61ef584163b864054a921
adupuis <adupuis@genymobile.com>
2011-07-23 19:26:36 +0200

    Ajout du widget pour les ajouts de congés.

---
+++ Web/backend/widgets/conges_add.dock.widget.php
@@ -0,0 +1,6 @@
+<li class="conges_add">
+	<a href="conges_add.php">
+		<span class="dock_item_title">Poser un congé</span><br/>
+		<span class="dock_item_content">Faire une demande de congés pour une période donnée.</span>
+	</a>
+</li>
