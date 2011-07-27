8ae3130d36c78896ad6b7407524d166703c1d96d
adupuis <adupuis@genymobile.com>
2011-07-27 09:25:18 +0200

    Modifications des homes pour utiliser les systèmes de widgets.

--- Web/home.php
+++ Web/home.php
@@ -17,55 +17,11 @@ include_once 'menu.php';
 	<ul>
 		<?php
 			include 'backend/widgets/notifications.dock.widget.php';
+			include 'backend/widgets/cra_add.dock.widget.php';
+			include 'backend/widgets/cra_validation.dock.widget.php';
+			include 'backend/widgets/conges_add.dock.widget.php';
+			include 'backend/widgets/conges_validation.dock.widget.php';
 		?>
-		<li class="idea_add">
-			<a href="idea_add.php">
-				<span class="dock_item_title">Ajouter une idée</span><br/>
-				<span class="dock_item_content">Ajouter une idée dans la boîte à idées.</span>
-			</a>
-		</li>
-		<li class="idea_edit">
-			<a href="idea_edit.php">
-				<span class="dock_item_title">Modifier une idée</span><br/>
-				<span class="dock_item_content">Modifier une idée existante.</span>
-			</a>
-		</li>
-		<li class="idea_remove">
-			<a href="idea_remove.php">
-				<span class="dock_item_title">Supprimer une idée</span><br/>
-				<span class="dock_item_content">Supprimer une idée existante.</span>
-			</a>
-		</li>
-		<li class="idea_list">
-			<a href="idea_list.php">
-				<span class="dock_item_title">Liste des idées</span><br/>
-				<span class="dock_item_content">Lister toutes les idées de la boîte à idée Geny Mobile.</span>
-			</a>
-		</li>
-		<li class="cra_add">
-			<a href="cra_add.php">
-				<span class="dock_item_title">Ajouter un CRA</span><br/>
-				<span class="dock_item_content">Ajouter un rapport d'activité pour une période donnée.</span>
-			</a>
-		</li>
-		<li class="cra_edit">
-			<a href="cra_validation.php">
-				<span class="dock_item_title">Valider des CRA</span><br/>
-				<span class="dock_item_content">Voir la liste des CRA non validés afin de les valider.</span>
-			</a>
-		</li>
-		<li class="conges_add">
-			<a href="conges_add.php">
-				<span class="dock_item_title">Poser un congé</span><br/>
-				<span class="dock_item_content">Faire une demande de congés pour une période donnée.</span>
-			</a>
-		</li>
-		<li class="conges_edit">
-			<a href="conges_edit.php">
-				<span class="dock_item_title">Modifier un congé</span><br/>
-				<span class="dock_item_content">Editer/modifier une demande de congés qui a déjà été faite.</span>
-			</a>
-		</li>
 	</ul>
 </div>
 
--- Web/home_conges.php
+++ Web/home_conges.php
@@ -16,30 +16,10 @@ include_once 'menu.php';
 
 <div id="maindock">
 	<ul>
-		<li class="conges_add">
-			<a href="conges_add.php">
-				<span class="dock_item_title">Poser un congé</span><br/>
-				<span class="dock_item_content">Faire une demande de congés pour une période donnée.</span>
-			</a>
-		</li>
-		<li class="conges_edit">
-			<a href="conges_edit.php">
-				<span class="dock_item_title">Modifier un congé</span><br/>
-				<span class="dock_item_content">Editer/modifier une demande de congés qui a déjà été faite.</span>
-			</a>
-		</li>
-		<li class="conges_remove">
-			<a href="conges_remove.php">
-				<span class="dock_item_title">Supprimer un congé</span><br/>
-				<span class="dock_item_content">Supprimer une demande de congés qui a déjà été faite.</span>
-			</a>
-		</li>
-		<li class="conges_list">
-			<a href="conges_list.php">
-				<span class="dock_item_title">Lister mes congés</span><br/>
-				<span class="dock_item_content">Afficher la liste de tous les congés que vous avez déjà posé ainsi que votre solde de congés.</span>
-			</a>
-		</li>
+		<?php
+			include 'backend/widgets/conges_add.dock.widget.php';
+			include 'backend/widgets/conges_validation.dock.widget.php';
+		?>
 	</ul>
 </div>
 
