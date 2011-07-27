504d646712b026d837a10707abf9f726ea1cb5d1
adupuis <adupuis@genymobile.com>
2011-07-27 09:24:37 +0200

    Modification mineurs dans les widgets de validation de CRAs et congés.

--- Web/backend/widgets/cra_validation.dock.widget.php
+++ Web/backend/widgets/cra_validation.dock.widget.php
@@ -1,4 +1,4 @@
-<li class="cra_admin_generic">
+<li class="cra_edit">
 	<a href="cra_validation.php">
 		<span class="dock_item_title">Validation CRA</span><br/>
 		<span class="dock_item_content">Validation des soumissions et des demandes de suppressions de CRAs.</span>
