0237ab868b4dad3e9955e7aaa5d91a67b4674a2d
adupuis <a.dupuis@infinityperl.org>
2011-07-31 13:56:59 +0200

    Ajout du widget pour accéder au reporting du mois précédent.

---
+++ Web/backend/widgets/reporting_previous_month_view.dock.widget.php
@@ -0,0 +1,6 @@
+<li class="reporting_monthly_view">
+	<a href="reporting_previous_month_view.php">
+		<span class="dock_item_title">Reporting M-1</span><br/>
+		<span class="dock_item_content">Reporting sur l'activité du mois précédent.</span>
+	</a>
+</li>
