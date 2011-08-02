53b145a4dda38793490bb4196cb0be70f94e1561
adupuis <a.dupuis@infinityperl.org>
2011-08-02 15:06:18 +0200

    Ajout d'une page permettant de lister tous les congés avec leur status. Ajout d'un widget d'accès à conges_list.php.

---
+++ Web/backend/widgets/conges_list.dock.widget.php
@@ -0,0 +1,6 @@
+<li class="conges_list">
+	<a href="conges_list.php">
+		<span class="dock_item_title">Liste des congés</span><br/>
+		<span class="dock_item_content">Liste de tous vos congés posés.</span>
+	</a>
+</li>
---
+++ Web/conges_list.php
@@ -0,0 +1,216 @@
+<?php
+// Variable to configure global behaviour
+$header_title = 'GENYMOBILE - Liste des CRA';
+$required_group_rights = 6;
+
+include_once 'header.php';
+include_once 'menu.php';
+
+$geny_ptr = new GenyProjectTaskRelation();
+$geny_tools = new GenyTools();
+date_default_timezone_set('Europe/Paris');
+$db_status = "";
+
+?>
+
+<div class="page_title">
+	<img src="images/<?php echo $web_config->theme ?>/conges.png"/><p>CRA</p>
+</div>
+
+
+<div id="mainarea">
+	<p class="mainarea_title">
+		<span class="conges_list">
+			Liste des congés
+		</span>
+	</p>
+	<p class="mainarea_content">
+		<p class="mainarea_content_intro">
+		Ce formulaire permet de lister tous les congés que vous avez posé.<br />
+		</p>
+		<script>
+			
+		
+		
+		
+		
+		
+		
+			(function($) {
+			/*
+			 * Function: fnGetColumnData
+			 * Purpose:  Return an array of table values from a particular column.
+			 * Returns:  array string: 1d data array 
+			 * Inputs:   object:oSettings - dataTable settings object. This is always the last argument past to the function
+			 *           int:iColumn - the id of the column to extract the data from
+			 *           bool:bUnique - optional - if set to false duplicated values are not filtered out
+			 *           bool:bFiltered - optional - if set to false all the table data is used (not only the filtered)
+			 *           bool:bIgnoreEmpty - optional - if set to false empty values are not filtered from the result array
+			 * Author:   Benedikt Forchhammer <b.forchhammer /AT\ mind2.de>
+			 */
+			$.fn.dataTableExt.oApi.fnGetColumnData = function ( oSettings, iColumn, bUnique, bFiltered, bIgnoreEmpty ) {
+				// check that we have a column id
+				if ( typeof iColumn == "undefined" ) return new Array();
+				
+				// by default we only wany unique data
+				if ( typeof bUnique == "undefined" ) bUnique = true;
+				
+				// by default we do want to only look at filtered data
+				if ( typeof bFiltered == "undefined" ) bFiltered = true;
+				
+				// by default we do not wany to include empty values
+				if ( typeof bIgnoreEmpty == "undefined" ) bIgnoreEmpty = true;
+				
+				// list of rows which we're going to loop through
+				var aiRows;
+				
+				// use only filtered rows
+				if (bFiltered == true) aiRows = oSettings.aiDisplay; 
+				// use all rows
+				else aiRows = oSettings.aiDisplayMaster; // all row numbers
+			
+				// set up data array	
+				var asResultData = new Array();
+				
+				for (var i=0,c=aiRows.length; i<c; i++) {
+					iRow = aiRows[i];
+					var aData = this.fnGetData(iRow);
+					var sValue = aData[iColumn];
+					
+					// ignore empty values?
+					if (bIgnoreEmpty == true && sValue.length == 0) continue;
+			
+					// ignore unique values?
+					else if (bUnique == true && jQuery.inArray(sValue, asResultData) > -1) continue;
+					
+					// else push the value onto the result data array
+					else asResultData.push(sValue);
+				}
+				
+				return asResultData;
+			}}(jQuery));
+
+
+			function fnCreateSelect( aData )
+			{
+				var r='<select><option value=""></option>', i, iLen=aData.length;
+				for ( i=0 ; i<iLen ; i++ )
+				{
+					r += '<option value="'+aData[i]+'">'+aData[i]+'</option>';
+				}
+				return r+'</select>';
+			}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			jQuery(document).ready(function(){
+				$("#formID").validationEngine('init');
+				// binds form submission and fields to the validation engine
+				$("#formID").validationEngine('attach');
+				
+				var oTable = $('#conges_list_table').dataTable( {
+					"bJQueryUI": true,
+					"bStateSave": true,
+					"bAutoWidth": false,
+					"sPaginationType": "full_numbers",
+					"oLanguage": {
+						"sSearch": "Recherche :",
+						"sLengthMenu": "Rapport par page _MENU_",
+						"sZeroRecords": "Aucun résultat",
+						"sInfo": "Aff. _START_ à _END_ de _TOTAL_ enregistrements",
+						"sInfoEmpty": "Aff. 0 à 0 de 0 enregistrements",
+						"sInfoFiltered": "(filtré de _MAX_ enregistrements)",
+						"oPaginate":{ 
+							"sFirst":"Début",
+							"sLast": "Fin",
+							"sNext": "Suivant",
+							"sPrevious": "Précédent"
+						}
+					}
+				} );
+				/* Add a select menu for each TH element in the table footer */
+				/* i+1 is to avoid the first row wich contains a <input> tag without any informations */
+				$("tfoot th").each( function ( i ) {
+					if( i == 1 || i == 2 || i == 6){
+						this.innerHTML = fnCreateSelect( oTable.fnGetColumnData(i) );
+						$('select', this).change( function () {
+							oTable.fnFilter( $(this).val(), i );
+						} );
+					}
+				} );
+			
+			});
+			
+			function onCheckBoxSelectAll(){
+				$("#conges_list_table").find(':checkbox').attr('checked', $('#chkBoxSelectAll').attr('checked'));
+			}
+		</script>
+		<?php
+			if( isset($db_status) && $db_status != "" ){
+				echo "<ul class=\"status_message\">\n$db_status\n</ul>";
+			}
+		?>
+		<script>
+			$(".status_message").click(function () {
+			$(".status_message").fadeOut("slow");
+			});
+		</script>
+		<form id="formID" action="#" method="post" class="table_container">
+			<p>
+				<table id="conges_list_table" style="color: black; width: 100%;">
+					<thead>
+						<th>Date</th>
+						<th>Projet</th>
+						<th>Tâche</th>
+						<th>Charge (tâche)</th>
+						<th>Status</th>
+					</thead>
+					<tbody>
+					<?php
+						$geny_ar = new GenyActivityReport();
+						$geny_ars = new GenyActivityReportStatus();
+						foreach( $geny_ar->getActivityReportsByProfileId( $profile->id ) as $ar ){
+							$tmp_activity = new GenyActivity( $ar->activity_id );
+							$tmp_ars = new GenyActivityReportStatus( $ar->status_id );
+							$tmp_task = new GenyTask( $tmp_activity->task_id );
+							$tmp_assignement = new GenyAssignement( $tmp_activity->assignement_id );
+							$tmp_project = new GenyProject( $tmp_assignement->project_id );
+							if( strripos($tmp_project->name,'congés') !== false ){
+								$geny_ars->loadActivityReportStatusById($ar->status_id);
+								echo "<tr><td class='centered'>".$tmp_activity->activity_date."</td><td class='centered'>".$tmp_project->name."</td><td class='centered'>".$tmp_task->name."</td><td class='centered'>".$tmp_activity->load."</td><td class='centered'>".$geny_ars->name."</td></tr>";
+							}
+						}
+					?>
+					</tbody>
+					<tfoot>
+						<th>Date</th>
+						<th>Projet</th>
+						<th>Tâche</th>
+						<th>Charge (tâche)</th>
+						<th>Status</th>
+					</tfoot>
+				</table>
+			</p>
+		</form>
+	</p>
+</div>
+
+<div id="bottomdock">
+	<ul>
+		<?php 
+			include 'backend/widgets/conges_add.dock.widget.php';
+			include 'backend/widgets/conges_validation.dock.widget.php';
+		?>
+	</ul>
+</div>
+
+<?php
+include_once 'footer.php';
+?>
