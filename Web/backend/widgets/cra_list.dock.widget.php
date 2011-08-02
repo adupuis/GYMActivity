9781b09d8e2a2870654e57837adb1ec4f680bc62
adupuis <a.dupuis@infinityperl.org>
2011-08-02 15:03:56 +0200

    Ajout d'une page permettant de lister tous les CRA avec leur status. Ajout d'un widget d'accès à cra_list.php.

---
+++ Web/backend/widgets/cra_list.dock.widget.php
@@ -0,0 +1,6 @@
+<li class="cra_list">
+	<a href="cra_list.php">
+		<span class="dock_item_title">Liste des CRA</span><br/>
+		<span class="dock_item_content">Liste de tous vos CRA créés.</span>
+	</a>
+</li>
