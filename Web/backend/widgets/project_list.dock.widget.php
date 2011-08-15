33cdd29fb92414384d36bb9d6d3dda8a104ed1e8
adupuis <adupuis@genymobile.com>
2011-08-16 00:45:28 +0200

    Modification de l'affichage du nom de l'entreprise pour utiliser GenyWebConfig::company_name au lieu d'une chaîne en dur.

--- Web/backend/widgets/project_list.dock.widget.php
+++ Web/backend/widgets/project_list.dock.widget.php
@@ -25,6 +25,6 @@
 <li class="project_list">
 	<a href="project_list.php">
 		<span class="dock_item_title">Liste des projets</span><br/>
-		<span class="dock_item_content">Lister tous les projets de GenY Mobile.</span>
+		<span class="dock_item_content">Lister tous les projets de <?php echo $web_config->company_name; ?>.</span>
 	</a>
 </li>
