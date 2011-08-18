6fb394a788a0b80b045660e809fbb500fb2fd976
adupuis <adupuis@genymobile.com>
2011-08-18 09:29:52 +0200

    Ajout d'un nouveau reporting : le reporting de charge permettant de visualiser la répartition de la charge entre 2 dates.

--- Web/backend/widgets/reporting_load.dock.widget.php
+++ Web/backend/widgets/reporting_load.dock.widget.php
@@ -1,4 +1,5 @@
 <?php
+
 //  Copyright (C) 2011 by GENYMOBILE & Arnaud Dupuis
 //  adupuis@genymobile.com
 //  http://www.genymobile.com
@@ -18,30 +19,11 @@
 //  Free Software Foundation, Inc.,
 //  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
 
-// Variable to configure global behaviour
-$header_title = '%COMPANY_NAME% - Reporting';
-$required_group_rights = 6;
-
-include_once 'header.php';
-include_once 'menu.php';
-
-
 ?>
 
-<div class="page_title">
-	<img src="images/default/reporting.png"/><p>Reporting</p>
-</div>
-
-<div id="maindock">
-	<ul>
-		<?php
-			include 'backend/widgets/reporting_monthly_view.dock.widget.php';
-			include 'backend/widgets/reporting_previous_month_view.dock.widget.php';
-			include 'backend/widgets/reporting_load.dock.widget.php';
-		?>
-	</ul>
-</div>
-
-<?php
-include_once 'footer.php';
-?>
+<li class="reporting_monthly_view">
+	<a href="reporting_load.php">
+		<span class="dock_item_title">Reporting de charge</span><br/>
+		<span class="dock_item_content">Reporting sur l'activité durant une période de temps et en terme de charge.</span>
+	</a>
+</li>
