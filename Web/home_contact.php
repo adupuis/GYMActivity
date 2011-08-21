548bb035a0b67b00853a592b0299707ef8a91c32
qdesert <qdesert@genymobile.com>
2011-08-21 20:06:08 +0200

    Ajout des widgets 'Liste des idées' et 'Ajouter une idée' dans la page 'Contact'.

--- Web/home_contact.php
+++ Web/home_contact.php
@@ -37,6 +37,8 @@ include_once 'menu.php';
 		<li class="">
 			<?php
 				include 'backend/widgets/send_notifications.dock.widget.php';
+				include 'backend/widgets/idea_list.dock.widget.php';
+ 				include 'backend/widgets/idea_add.dock.widget.php';
 			?>
 		</li>
 	</ul>
