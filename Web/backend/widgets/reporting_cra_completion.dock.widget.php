192a6df898b83462abd55c06908aece80fb988bc
adupuis <a.dupuis@infinityperl.org>
2011-08-27 13:50:23 +0200

    Correction du lien du widget reporting_cra_completion

--- Web/backend/widgets/reporting_cra_completion.dock.widget.php
+++ Web/backend/widgets/reporting_cra_completion.dock.widget.php
@@ -22,7 +22,7 @@
 ?>
 
 <li class="reporting_cra">
-	<a href="reporting_load.php">
+	<a href="reporting_cra_completion.php">
 		<span class="dock_item_title">Complétion des CRA</span><br/>
 		<span class="dock_item_content">Liste des profils est de l'état du remplissage des CRA.</span>
 	</a>
