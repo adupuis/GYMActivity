59294ac7195e7469c691972b49855656e8e9bb96
adupuis <adupuis@genymobile.com>
2011-10-22 15:37:53 +0200

    Ajout des Properties à la base.
    - Ajout des Properties au script de création de la base de données.
    - Ajout du scipt d'update de la base de données vers la version 0.4.9.
    - Ajout des clauses de suppressions des tables dans le script de nettoyage.

--- Web/create_database.sql
+++ Web/create_database.sql
@@ -357,4 +357,62 @@ CREATE TABLE IdeaVotes (
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 ALTER TABLE IdeaVotes AUTO_INCREMENT=1;
 
+-- Properties
+
+CREATE TABLE PropertyTypes (
+	property_type_id int auto_increment,
+	property_type_shortname varchar(250) not null default 'P_TYPE',
+	property_type_name varchar(250) not null default 'Property type name',
+	primary key(property_type_id)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+ALTER TABLE PropertyTypes AUTO_INCREMENT=1;
+
+INSERT INTO PropertyTypes VALUES(0,'PROP_TYPE_BOOL','Une propriété booléenne (vrai/faux).');
+INSERT INTO PropertyTypes VALUES(0,'PROP_TYPE_MULTI_SELECT','Une propriété contenant un choix multiple.');
+INSERT INTO PropertyTypes VALUES(0,'PROP_TYPE_LIST_SELECT','Une propriété contenant un choix unique dans une liste.');
+INSERT INTO PropertyTypes VALUES(0,'PROP_TYPE_SHORT_TEXT','Une propriété contenant un text court.');
+INSERT INTO PropertyTypes VALUES(0,'PROP_TYPE_LONG_TEXT','Une propriété contenant un text long.');
+INSERT INTO PropertyTypes VALUES(0,'PROP_TYPE_DATE','Une propriété contenant une date.');
+
+CREATE TABLE Properties (
+	property_id int auto_increment,
+	property_name varchar(250) not null default 'PNAME',
+	property_label varchar(250) not null default 'Property name',
+	property_type_id int not null,
+	primary key(property_id),
+	foreign key(property_type_id) references PropertyTypes(property_type_id) ON DELETE CASCADE
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+ALTER TABLE Properties AUTO_INCREMENT=1;
+
+-- Exemple de propriété
+INSERT INTO Properties VALUES(0,'PROP_LIVE_DEBUG','Activer/desactiver le debug en live.',1);
+
+-- Version du schéma de la base de donnée
+INSERT INTO Properties VALUES(0,'PROP_DB_VERSION','Version du schéma de la base de données.',4);
+
+CREATE TABLE PropertyOptions (
+	property_option_id int auto_increment,
+	property_option_content text not null,
+	property_id int not null,
+	primary key(property_option_id),
+	foreign key(property_id) references Properties(property_id) ON DELETE CASCADE
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+ALTER TABLE PropertyOptions AUTO_INCREMENT=1;
+
+-- Suite de l'exemple
+INSERT INTO PropertyOptions VALUES(0,'Activé',1);
+INSERT INTO PropertyOptions VALUES(0,'Désactivé',1);
+
+-- C'est dans cette table que vont les valeurs séléctionnées. Dans l'exemple ci-dessus il y aurait 1 ou 2 (les id d'une des deux options possible)
+CREATE TABLE PropertyValues (
+	property_value_id int auto_increment,
+	property_id int not null,
+	property_value_content text not null,
+	primary key(property_value_id),
+	foreign key(property_id) references Properties(property_id) ON DELETE CASCADE
+) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+ALTER TABLE PropertyValues AUTO_INCREMENT=1;
+
+INSERT INTO PropertyValues VALUES(0,2,'4');
+
 COMMIT;
