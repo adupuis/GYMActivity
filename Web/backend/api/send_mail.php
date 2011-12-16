bf2f0b45f22df75c09fdef8a4364c1b020a67e24
adupuis <adupuis@genymobile.com>
2011-12-16 09:03:27 +0100

    Support des destinataires multiples dans send_mail.php. - ajout du support des destinataires multiples (1 mail par destinataire) - code cleanup

--- Web/backend/api/send_mail.php
+++ Web/backend/api/send_mail.php
@@ -34,25 +34,10 @@ try {
 	if($auth_granted){
 		$tmp_profile = new GenyProfile();
 		$web_config = new GenyWebConfig();
-
-		$month = date('m', time());
-		$year=date('Y', time());
-
-		$start_date = GenyTools::getCurrentMonthFirstDayDate();
-		$end_date = GenyTools::getCurrentMonthLastDayDate();
-
-		$geny_ar = new GenyActivityReport();
-		$worked_days = GenyTools::getWorkedDaysList(strtotime($start_date),strtotime($end_date));
-		$estimated_load=0;
-		foreach( $worked_days as $day ){
-			$estimated_load += 8;
-		}
-
-		$user_completion_data = array();
 		
 		$from = getParam("from","CRA Admin <admin@genymobile.com>");
 		$subject = getParam("subject","empty subject");
-		$to = getParam("to");
+		$to_raw = getParam("to");
 		
 
 		$host = "ssl://smtp.gmail.com";
@@ -63,63 +48,28 @@ try {
 		$json_messages = array();
 		
 		$body = "<html><body>".getParam("body","no body defined.")."</body></html>";
-		$headers = array ('From' => $from,
-			'To' => $to,
-			'Subject' => $subject,
-			'Content-Type' => 'text/html; charset=UTF-8');
-			$smtp = Mail::factory('smtp',
-			array ('host' => $host,
-			'port' => $port,
-			'auth' => true,
-			'username' => $username,
-			'password' => $password)
-		);
+		foreach( explode(",",$to_raw) as $to ){
+			$headers = array ('From' => $from,
+				'To' => $to,
+				'Subject' => $subject,
+				'Content-Type' => 'text/html; charset=UTF-8');
+				$smtp = Mail::factory('smtp',
+				array ('host' => $host,
+				'port' => $port,
+				'auth' => true,
+				'username' => $username,
+				'password' => $password)
+			);
 
-		$mail = $smtp->send($to, $headers, $body);
+			$mail = $smtp->send($to, $headers, $body);
 
-		if (PEAR::isError($mail)) {
-			$json_messages[] = array("status" => "error", "status_message" => $mail->getMessage() );
-		} else {
-			$json_messages[] = array("status" => "success", "status_message" => "Message successfully sent." );
+			if (PEAR::isError($mail)) {
+				$json_messages[] = array("status" => "error", "status_message" => "Error while sending message to $to: ".$mail->getMessage() );
+			} else {
+				$json_messages[] = array("status" => "success", "status_message" => "Message successfully sent to $to." );
+			}
 		}
 
-// 		foreach( $tmp_profile->getProfilesListWithRestrictions( array("rights_group_id=3","profile_is_active=true") ) as $p ){
-// 			$user_load=0;
-// 			foreach( $worked_days as $day ){
-// 				$user_load += $geny_ar->getDayLoad($p->id,$day);
-// 			}
-// 			if( $user_load < $estimated_load ){
-// 				$to = "<".$p->email.">";
-// 				$hours = $estimated_load-$user_load ;
-// 				$hours_string = "";
-// 				if( $hours <= 10 )
-// 					$hours_string = "<font style='color:darkblue;'>$hours</font>";
-// 				else if( $hours <= 30 )
-// 					$hours_string = "<font style='color:orange;'>$hours</font>";
-// 				else
-// 					$hours_string = "<font style='color:red;'>$hours</font>";
-// 				$body = "<html><body><p>Bonjour ".$p->firstname.",<br/><br/>A ce jour vos CRA du <strong>$month/$year</strong> sont incomplets. Merci de saisir dès aujourd'hui l'ensemble des ".$hours_string." heures manquantes (soit ".($hours/8)." jours).<br/>Vous pouvez saisir les CRA manquants sur <a href='http://cra.genymobile.com'>http://cra.genymobile.com</a>.<br/><br/>---<br/><font color=\"#7f7f7f\" face=\"&#39;BN Year 2000&#39;\" size=\"6\"><span style=\"line-height:36px\"><img src=\"https://lh5.googleusercontent.com/-D4J1fAOyk8A/TgbxwOsSIjI/AAAAAAAAABE/zWEpLl0Q3ZM/s144/genymobile-24.png\"><br></span></font><br/><font color=\"#666666\">Gestion de CRA</font></p><br/></body></html>";
-// 				$headers = array ('From' => $from,
-// 				'To' => $to,
-// 				'Subject' => $subject,
-// 				'Content-Type' => 'text/html; charset=UTF-8');
-// 				$smtp = Mail::factory('smtp',
-// 				array ('host' => $host,
-// 				'port' => $port,
-// 				'auth' => true,
-// 				'username' => $username,
-// 				'password' => $password));
-// 
-// 				$mail = $smtp->send($to, $headers, $body);
-// 
-// 				if (PEAR::isError($mail)) {
-// 					$json_messages[] = array("status" => "error", "status_message" => $mail->getMessage() );
-// 				} else {
-// 					$json_messages[] = array("status" => "success", "status_message" => "Message successfully sent." );
-// 				}
-// 			}
-// 			
-// 		}
 		echo json_encode($json_messages);
 	}
 } catch (Exception $e) {
