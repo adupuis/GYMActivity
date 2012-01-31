bc1c751d4291deef3a8982436296914774c25a5e
Quentin Désert <qdesert@genymobile.com>
2012-01-31 09:58:23 +0100

    Ajout du widget de création de page Intranet.

--- Web/submodules/home_admin.php
+++ Web/submodules/home_admin.php
@@ -27,6 +27,7 @@
 			include 'backend/widgets/intranet_type_add.dock.widget.php';
 			include 'backend/widgets/intranet_type_list.dock.widget.php';
 			include 'backend/widgets/intranet_tag_list.dock.widget.php';
+			include 'backend/widgets/intranet_page_add.dock.widget.php';
 			include 'backend/widgets/intranet_page_list.dock.widget.php';
 		?>
 	</ul>
