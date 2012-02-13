f5e78f04775762f269f31719189fc4e1ebd17229
Quentin Désert <qdesert@genymobile.com>
2012-02-13 13:14:08 +0100

    Début de l'implémentation de la page d'accueil de l'Intranet
    - Création dynamique et affichage des widgets des catégories existantes

--- Web/submodules/home_intranet.php
+++ Web/submodules/home_intranet.php
@@ -1,7 +1,20 @@
 <div id="maindock">
 	<ul>
 		<?php
-			// list of category widgets
+			$geny_intranet_category = new GenyIntranetCategory();
+			foreach( $geny_intranet_category->getAllIntranetCategories() as $intranet_category ) {
+				$filename = "images/genymobile-2012/".html_entity_decode( $intranet_category->image_name ).".png";
+				if( !file_exists( $filename ) ) {
+					$filename = "images/genymobile-2012/intranet_category_generic.png";
+				}
+				
+				echo "<li class=\"intranet_category_".$intranet_category->id."\">";
+				echo "<a href=# style=\"background-image: url(".$filename.")\">";
+				echo "<span class=\"dock_item_title\">".$intranet_category->name."</span><br/>";
+				echo "<span class=\"dock_item_content\">".$intranet_category->description."</span>";
+				echo "</a>";
+				echo "</li>";
+			}
 		?>
 	</ul>
 </div>
