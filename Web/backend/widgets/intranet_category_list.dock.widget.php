a2c014b10d81c7ea9dbf334e19cf637bb88e606a
Quentin Désert <qdesert@genymobile.com>
2012-02-13 13:06:39 +0100

    Renommage du widget de liste des catégories (ajout d'un 's')

--- Web/backend/widgets/intranet_category_list.dock.widget.php
+++ Web/backend/widgets/intranet_category_list.dock.widget.php
@@ -23,7 +23,7 @@
 
 <li class="intranet_category_list">
 	<a href="loader.php?module=intranet_category_list">
-		<span class="dock_item_title">Cat. Intranet</span><br/>
+		<span class="dock_item_title">Cats. Intranet</span><br/>
 		<span class="dock_item_content">Lister toutes les catégories de l'Intranet.</span>
 	</a>
 </li>
