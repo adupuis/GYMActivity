f62a291840db6362421b533dbf36160af7d8532b
Quentin Désert <qdesert@genymobile.com>
2012-02-15 16:56:40 +0100

    Modification de la Home Intranet pour pouvoir accéder directement à une catégorie depuis le menu genymobile2012

--- Web/submodules/home_intranet.php
+++ Web/submodules/home_intranet.php
@@ -1,5 +1,7 @@
 <?php
 
+$gritter_notifications = array();
+
 $geny_intranet_category = new GenyIntranetCategory();
 $geny_intranet_type = new GenyIntranetType();
 $geny_intranet_tag = new GenyIntranetTag();
@@ -7,120 +9,9 @@ $geny_intranet_page = new GenyIntranetPage();
 
 ?>
 
-<style>
-	@import "styles/genymobile-2012/home_intranet.css";
-</style>
-
-<div id="home_intranet_dock">
-
-	<p id="home_intranet_tag_list">
-		<label for="intranet_tag_select">Tags</label>
-		<select name="intranet_tag_select[]" id="intranet_tag_select" multiple class="tag_list chzn-select" data-placeholder="Choisissez un ou plusieurs tags...">
-		</select>
-	</p>
-	<p id="home_intranet_category_list" style="display:none">
-		<label for="intranet_category_select">Catégories</label>
-		<select name="intranet_category_select" id="intranet_category_select" class="category_type_list chzn-select">
-			<option value="0"></option>
-			<?php
-				foreach( $geny_intranet_category->getAllIntranetCategories() as $intranet_category ) {
-					echo "<option value=\"".$intranet_category->id."\">".$intranet_category->name."</option>\n";
-				}
-			?>
-		</select>
-	</p>
-	<p id="home_intranet_type_list" style="display:none">
-		<label for="intranet_type_select">Sous-catégories</label>
-		<select name="intranet_type_select" id="intranet_type_select" class="category_type_list chzn-select">
-			<option value="0"></option>
-		</select>
-	</p>
-
-	<div id="home_intranet_categories">
-		<div class="home_intranet_separator">Catégories</div>
-		<ul>
-			<?php
-				$geny_intranet_category = new GenyIntranetCategory();
-				foreach( $geny_intranet_category->getAllIntranetCategories() as $intranet_category ) {
-					$filename = "images/genymobile-2012/".html_entity_decode( $intranet_category->image_name ).".png";
-					if( !file_exists( $filename ) ) {
-						$filename = "images/genymobile-2012/intranet_category_generic.png";
-					}
-									
-					echo "<li class=\"widget\">";
-					echo "<a class=\"widget_link\" href=# style=\"background-image: url(".$filename.")\" onclick=\"displayIntranetTypeWidgets( ".$intranet_category->id.");updateIntranetCategoryList( ".$intranet_category->id.");\">";
-					echo "<span class=\"dock_item_title\">".$intranet_category->name."</span><br/>";
-					echo "<span class=\"dock_item_content\">".$intranet_category->description."</span>";
-					echo "</a>";
-					echo "</li>\n";
-				}
-			?>
-		</ul>
-	</div>
-	
-	<div id="home_intranet_types" style="display:none">
-		<div class="home_intranet_separator">Sous-catégories</div>
-		<ul>
-			<?php
-				foreach( $geny_intranet_type->getAllIntranetTypes() as $intranet_type ) {
-					$intranet_category = new GenyIntranetCategory( $intranet_type->intranet_category_id );
-					$filename = "images/genymobile-2012/".html_entity_decode( $intranet_category->image_name ).".png";
-					if( !file_exists( $filename ) ) {
-						$filename = "images/genymobile-2012/intranet_category_generic.png";
-					}
-					
-					echo "<li class=\"widget intranet_type type_intranet_category_".$intranet_category->id."\">";
-					echo "<a class=\"widget_link\" href=# style=\"background-image: url(".$filename.")\" onclick=\"displayIntranetPageWidgets( ".$intranet_type->id.");updateIntranetTypeList( ".$intranet_type->id.");\">";
-					echo "<span class=\"dock_item_title\">".$intranet_type->name."</span><br/>";
-					echo "<span class=\"dock_item_content\">".$intranet_type->description."</span>";
-					echo "</a>";
-					echo "</li>\n";
-				}
-			?>
-		</ul>
-	</div>
-	
-	<div id="home_intranet_pages" style="display:none">
-		<div class="home_intranet_separator">Pages</div>
-		<ul>
-			<?php
-				foreach( $geny_intranet_page->getAllIntranetPages() as $intranet_page ) {
-					$intranet_category = new GenyIntranetCategory( $intranet_page->intranet_category_id );
-					$intranet_type = new GenyIntranetType( $intranet_page->intranet_type_id );
-					$filename = "images/genymobile-2012/".html_entity_decode( $intranet_category->image_name ).".png";
-					if( !file_exists( $filename ) ) {
-						$filename = "images/genymobile-2012/intranet_category_generic.png";
-					}
-					
-					$intranet_tag_list = $geny_intranet_tag->getIntranetTagsByPage( $intranet_page->id );
-					$page_intranet_tag_classes = "";
-					foreach( $intranet_tag_list as $tag ) {
-						$page_intranet_tag_classes .= " page_intranet_tag_".$tag->id;
-					}
-					$page_intranet_tag_names = "<br/>";
-					foreach( $intranet_tag_list as $tag ) {
-						$page_intranet_tag_names .= " ".$tag->name;
-					}
-					
-					echo "<li class=\"widget intranet_page page_intranet_category_".$intranet_category->id." page_intranet_type_".$intranet_type->id.$page_intranet_tag_classes."\">";
-					echo "<a class=\"widget_link\" href=\"loader.php?module=intranet_page_view&load_intranet_page=true&intranet_page_id=".$intranet_page->id."\" style=\"background-image: url(".$filename.")\">";
-					echo "<span class=\"dock_item_title\">".$intranet_page->title."</span><br/>";
-					echo "<span class=\"dock_item_content\">".$intranet_page->description."</span>";
-					echo "<span class=\"dock_item_content\">".$page_intranet_tag_names."</span>";
-					echo "</a>";
-					echo "</li>\n";
-				}
-			?>
-		</ul>
-	</div>
-</div>
-
 <script type="text/javascript">
 	
 	getIntranetTags();
-	$("#intranet_tag_select").change( intranetTagChanged );
-	$("#intranet_category_select").change( intranetCategoryChanged );
-	$("#intranet_type_select").change( intranetTypeChanged );
 	
 	function getIntranetTags() {
 		console.log( 'function getIntranetTags' );
@@ -278,6 +169,135 @@ $geny_intranet_page = new GenyIntranetPage();
 
 </script>
 
+<style>
+	@import "styles/genymobile-2012/home_intranet.css";
+</style>
+
+<div id="home_intranet_dock">
+
+	<p id="home_intranet_tag_list">
+		<label for="intranet_tag_select">Tags</label>
+		<select name="intranet_tag_select[]" id="intranet_tag_select" multiple class="tag_list chzn-select" data-placeholder="Choisissez un ou plusieurs tags...">
+		</select>
+	</p>
+	<p id="home_intranet_category_list" style="display:none">
+		<label for="intranet_category_select">Catégories</label>
+		<select name="intranet_category_select" id="intranet_category_select" class="category_type_list chzn-select" data-placeholder="Choisissez une catégorie...">
+			<option value="0"></option>
+			<?php
+				foreach( $geny_intranet_category->getAllIntranetCategories() as $intranet_category ) {
+					echo "<option value=\"".$intranet_category->id."\">".$intranet_category->name."</option>\n";
+				}
+			?>
+		</select>
+	</p>
+	<p id="home_intranet_type_list" style="display:none">
+		<label for="intranet_type_select">Sous-catégories</label>
+		<select name="intranet_type_select" id="intranet_type_select" class="category_type_list chzn-select">
+			<option value="0"></option>
+		</select>
+	</p>
+
+	<div id="home_intranet_categories">
+		<div class="home_intranet_separator">Catégories</div>
+		<ul>
+			<?php
+				$geny_intranet_category = new GenyIntranetCategory();
+				foreach( $geny_intranet_category->getAllIntranetCategories() as $intranet_category ) {
+					$filename = "images/genymobile-2012/".html_entity_decode( $intranet_category->image_name ).".png";
+					if( !file_exists( $filename ) ) {
+						$filename = "images/genymobile-2012/intranet_category_generic.png";
+					}
+									
+					echo "<li class=\"widget\">";
+					echo "<a class=\"widget_link\" href=# style=\"background-image: url(".$filename.")\" onclick=\"displayIntranetTypeWidgets( ".$intranet_category->id.");updateIntranetCategoryList( ".$intranet_category->id.");\">";
+					echo "<span class=\"dock_item_title\">".$intranet_category->name."</span><br/>";
+					echo "<span class=\"dock_item_content\">".$intranet_category->description."</span>";
+					echo "</a>";
+					echo "</li>\n";
+				}
+			?>
+		</ul>
+	</div>
+	
+	<div id="home_intranet_types" style="display:none">
+		<div class="home_intranet_separator">Sous-catégories</div>
+		<ul>
+			<?php
+				foreach( $geny_intranet_type->getAllIntranetTypes() as $intranet_type ) {
+					$intranet_category = new GenyIntranetCategory( $intranet_type->intranet_category_id );
+					$filename = "images/genymobile-2012/".html_entity_decode( $intranet_category->image_name ).".png";
+					if( !file_exists( $filename ) ) {
+						$filename = "images/genymobile-2012/intranet_category_generic.png";
+					}
+					
+					echo "<li class=\"widget intranet_type type_intranet_category_".$intranet_category->id."\">";
+					echo "<a class=\"widget_link\" href=# style=\"background-image: url(".$filename.")\" onclick=\"displayIntranetPageWidgets( ".$intranet_type->id.");updateIntranetTypeList( ".$intranet_type->id.");\">";
+					echo "<span class=\"dock_item_title\">".$intranet_type->name."</span><br/>";
+					echo "<span class=\"dock_item_content\">".$intranet_type->description."</span>";
+					echo "</a>";
+					echo "</li>\n";
+				}
+			?>
+		</ul>
+	</div>
+	
+	<div id="home_intranet_pages" style="display:none">
+		<div class="home_intranet_separator">Pages</div>
+		<ul>
+			<?php
+				foreach( $geny_intranet_page->getAllIntranetPages() as $intranet_page ) {
+					$intranet_category = new GenyIntranetCategory( $intranet_page->intranet_category_id );
+					$intranet_type = new GenyIntranetType( $intranet_page->intranet_type_id );
+					$filename = "images/genymobile-2012/".html_entity_decode( $intranet_category->image_name ).".png";
+					if( !file_exists( $filename ) ) {
+						$filename = "images/genymobile-2012/intranet_category_generic.png";
+					}
+					
+					$intranet_tag_list = $geny_intranet_tag->getIntranetTagsByPage( $intranet_page->id );
+					$page_intranet_tag_classes = "";
+					foreach( $intranet_tag_list as $tag ) {
+						$page_intranet_tag_classes .= " page_intranet_tag_".$tag->id;
+					}
+					$page_intranet_tag_names = "<br/>";
+					foreach( $intranet_tag_list as $tag ) {
+						$page_intranet_tag_names .= " ".$tag->name;
+					}
+					
+					echo "<li class=\"widget intranet_page page_intranet_category_".$intranet_category->id." page_intranet_type_".$intranet_type->id.$page_intranet_tag_classes."\">";
+					echo "<a class=\"widget_link\" href=\"loader.php?module=intranet_page_view&load_intranet_page=true&intranet_page_id=".$intranet_page->id."\" style=\"background-image: url(".$filename.")\">";
+					echo "<span class=\"dock_item_title\">".$intranet_page->title."</span><br/>";
+					echo "<span class=\"dock_item_content\">".$intranet_page->description."</span>";
+					echo "<span class=\"dock_item_content\">".$page_intranet_tag_names."</span>";
+					echo "</a>";
+					echo "</li>\n";
+				}
+			?>
+		</ul>
+	</div>
+</div>
+
+<script type="text/javascript">
+	
+	$("#intranet_tag_select").change( intranetTagChanged );
+	$("#intranet_category_select").change( intranetCategoryChanged );
+	$("#intranet_type_select").change( intranetTypeChanged );
+	
+</script>
+
 <?php
-	$bottomdock_items = array('backend/widgets/notifications.dock.widget.php','backend/widgets/intranet_page_add.dock.widget.php');
+	
+$load_intranet_category = GenyTools::getParam( 'load_intranet_category', 'NULL' );
+
+if( $load_intranet_category == 'true' ) {
+	$intranet_category_id = GenyTools::getParam( 'intranet_category_id', 'NULL' );
+	if( $intranet_category_id != 'NULL' ) {
+		echo "<script type=\"text/javascript\">displayIntranetTypeWidgets( ".$intranet_category_id.");updateIntranetCategoryList( ".$intranet_category_id.");</script>";
+	}
+	else {
+// 		$gritter_notifications[] = array( 'status'=>'error', 'title' => 'Impossible de charger la catégorie Intranet','msg'=>"id non spécifié." );
+	}
+}
+
+$bottomdock_items = array('backend/widgets/notifications.dock.widget.php','backend/widgets/intranet_page_add.dock.widget.php');
 ?>
