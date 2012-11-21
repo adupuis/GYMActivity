8cdc50097f204f0953ecc8d486bc1cba29252b1d
adupuis <adupuis@genymobile.com>
2012-11-21 08:04:29 +0100

    Ajout initial du reporting_conges
    - reporting pour générer le fichier de congés pour Marie.

---
+++ Web/submodules/reporting_conges.php
@@ -0,0 +1,8 @@
+<?php
+
+// Pour le reporting des congés il faut générer le fichier Excel pour Marie.
+// Utilisation de Spreadsheet_Excel_Writer :
+// http://pear.php.net/manual/fr/package.fileformats.spreadsheet-excel-writer.intro.php
+// pear install Spreadsheet_Excel_Writer
+
+?>
