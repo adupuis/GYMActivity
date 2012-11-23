530683a95fd09f2e8842cc4d33b739c664a0e9e7
adupuis <adupuis@genymobile.com>
2012-11-23 08:09:38 +0100

    Ajout d'un widget pour la génération du rapport de congés

---
+++ Web/backend/widgets/conges_generate_report.dock.widget.php
@@ -0,0 +1,29 @@
+<?php
+
+//  Copyright (C) 2011 by GENYMOBILE & Arnaud Dupuis
+//  adupuis@genymobile.com
+//  http://www.genymobile.com
+// 
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the
+//  Free Software Foundation, Inc.,
+//  59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+
+?>
+
+<li class="conges_list">
+	<a href="submodules/generate_conges_report.php" target="_blank">
+		<span class="dock_item_title">Report Excel Congés</span><br/>
+		<span class="dock_item_content">Génère un rapport Excel contenant la liste des congés du mois en cours.</span>
+	</a>
+</li>
