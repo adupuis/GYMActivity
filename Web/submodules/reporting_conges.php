88246f44fb83f5b7af808636c43bf93d3071e241
adupuis <adupuis@genymobile.com>
2012-11-23 08:01:27 +0100

    Version presque-finale du générateur de rapport de congés
    - Une feuille par type de congés
    - Echappement des caractère UTF8 (pas tous)
    - Création de feuilles à la volée
    - Correction du bug des indexes

--- Web/submodules/reporting_conges.php
+++ Web/submodules/reporting_conges.php
@@ -4,25 +4,37 @@
 // Utilisation de Spreadsheet_Excel_Writer :
 // http://pear.php.net/manual/fr/package.fileformats.spreadsheet-excel-writer.intro.php
 // pear install Spreadsheet_Excel_Writer
-
+date_default_timezone_set('Europe/Paris');
 require_once 'Spreadsheet/Excel/Writer.php';
 include_once '../classes/GenyWebConfig.php';
 include_once '../classes/GenyActivityReportWorkflow.php';
 include_once '../classes/GenyActivityReportStatus.php';
+include_once '../classes/GenyTools.php';
+
+function convertToUtf8($string) {
+	$new_string = str_replace("/","\x2F",$string);
+	$new_string = str_replace("é","\xE9",$new_string);
+	$new_string = str_replace("è","\xE8",$new_string);
+	$new_string = str_replace("ê","\xEA",$new_string);
+	$new_string = str_replace("ë","\xEA",$new_string);
+	$new_string = str_replace("<","\x3C",$new_string);
+	$new_string = str_replace(">","\x3E",$new_string);
+	$new_string = str_replace("-","\x2D",$new_string);
+	
+	return $new_string;
+}
+
 
 $web_config = new GenyWebConfig();
 $month = date('m', time());
 $year=date('Y', time());
 $filename = $web_config->company_name."-Recapitulatif-conges-$month-$year.xls";
+$worksheets = array();
+$worksheets_indexes = array();
 
 // Création d'un manuel de travail
 $workbook = new Spreadsheet_Excel_Writer();
 // $workbook->setVersion(8);
-
-// Création d'une feuille de travail
-$worksheet =& $workbook->addWorksheet('Congés');
-$worksheet->setInputEncoding('utf-8');
-$worksheet->setColumn(0,4,30);
 
 // Format 
 // Couleurs Geny
@@ -39,15 +51,9 @@ $format_title->setVAlign('vcenter');
 $format_center =& $workbook->addFormat();
 $format_center->setHAlign('center');
 
-// On affiche les headers
-$worksheet->write(0, 0, 'Collaborateur',$format_title);
-$worksheet->write(0, 1, 'Date',$format_title);
-$worksheet->write(0, 2, "Type de cong\xE9s",$format_title);
-$worksheet->write(0, 3, "Soci\xE9t\xE9",$format_title);
-$worksheet->write(0, 4, 'Charge en jour',$format_title);
-
+// Récupération des status intéressant
 $activity_report_workflow = new GenyActivityReportWorkflow();
-$workflow = $activity_report_workflow->getActivityReportsWorkflow();
+$workflow = $activity_report_workflow->getActivityReportsWorkflowWithRestrictions( array( "activity_date >= '".GenyTools::getCurrentMonthFirstDayDate()."'", "activity_date <= '".GenyTools::getCurrentMonthLastDayDate()."'" ) );
 $geny_ars_approved = new GenyActivityReportStatus();
 $geny_ars_approved->loadActivityReportStatusByShortName('APPROVED');
 $geny_ars_billed = new GenyActivityReportStatus();
@@ -61,12 +67,26 @@ foreach($workflow as $row) {
 	// Oh mon dieu et dire que j'ose écrire ça et que ça me fait même sourrire !
 	// TODO: Il faudra quand même mettre 'Congés' dans une property
 	if( $row->project_name == 'Congés' && ($row->activity_report_status_id == $geny_ars_approved->id || $row->activity_report_status_id == $geny_ars_billed->id || $row->activity_report_status_id == $geny_ars_paid->id || $row->activity_report_status_id == $geny_ars_close->id) ){
-		$worksheet->write(1, 0, "$row->profile_lastname $row->profile_firstname",$format_center);
-		$worksheet->write(1, 1, $row->activity_date,$format_center);
-		$worksheet->write(1, 2, $row->task_name,$format_center);
-		$worksheet->write(1, 3, $row->client_name,$format_center);
-		$worksheet->write(1, 4, $row->activity_load/8,$format_center);
-		
+		if( ! array_key_exists($row->task_name, $worksheets) ) {
+			// Création d'une feuille de travail
+			$worksheet =& $workbook->addWorksheet(convertToUtf8($row->task_name));
+			$worksheet->setInputEncoding('utf-8');
+			$worksheet->setColumn(0,4,30);
+			// On affiche les headers
+			$worksheet->write(0, 0, 'Collaborateur',$format_title);
+			$worksheet->write(0, 1, 'Date',$format_title);
+			$worksheet->write(0, 2, "Type de cong\xE9s",$format_title);
+			$worksheet->write(0, 3, "Soci\xE9t\xE9",$format_title);
+			$worksheet->write(0, 4, 'Charge en jour',$format_title);
+			$worksheets[$row->task_name] = $worksheet;
+			$worksheets_indexes[$row->task_name] = 1;
+		}
+		$worksheets[$row->task_name]->write($worksheets_indexes[$row->task_name], 0, convertToUtf8("$row->profile_lastname $row->profile_firstname"),$format_center);
+		$worksheets[$row->task_name]->write($worksheets_indexes[$row->task_name], 1, $row->activity_date,$format_center);
+		$worksheets[$row->task_name]->write($worksheets_indexes[$row->task_name], 2, convertToUtf8($row->task_name),$format_center);
+		$worksheets[$row->task_name]->write($worksheets_indexes[$row->task_name], 3, convertToUtf8($row->client_name),$format_center);
+		$worksheets[$row->task_name]->write($worksheets_indexes[$row->task_name], 4, $row->activity_load/8,$format_center);
+		$worksheets_indexes[$row->task_name]++;
 	}
 }
 
