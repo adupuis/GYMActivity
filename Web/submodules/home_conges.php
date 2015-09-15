b291569da8e5815d9144acde5e80f57283c71660
Jeremy Vagnet <jvagnet@genymobile.com>
2015-09-15 16:42:53 +0200

    Update home_conges.php

--- Web/submodules/home_conges.php
+++ Web/submodules/home_conges.php
@@ -1,16 +1,18 @@
 <div id="maindock">
 	<ul>
 		<?php
-			include 'backend/widgets/conges_add.dock.widget.php';
-			include 'backend/widgets/conges_validation.dock.widget.php';
-			include 'backend/widgets/conges_list.dock.widget.php';
-			include 'backend/widgets/conges_deletion.dock.widget.php';
-			include 'backend/widgets/conges_view.dock.widget.php';
-			if( $profile->rights_group_id == 1 || $profile->rights_group_id == 2 ){
-				include 'backend/widgets/holiday_summary_add.dock.widget.php';
-				include 'backend/widgets/holiday_summary_list.dock.widget.php';
-				include 'backend/widgets/conges_generate_report.dock.widget.php';
-			}
+			if( $profile->rights_group_id != 6 ){
+                                include 'backend/widgets/conges_add.dock.widget.php';
+                                include 'backend/widgets/conges_validation.dock.widget.php';
+                                include 'backend/widgets/conges_list.dock.widget.php';
+                                include 'backend/widgets/conges_deletion.dock.widget.php';
+                        }
+                        if( $profile->rights_group_id == 1 || $profile->rights_group_id == 2 ){
+                                include 'backend/widgets/holiday_summary_add.dock.widget.php';
+                                include 'backend/widgets/holiday_summary_list.dock.widget.php';
+                                include 'backend/widgets/conges_generate_report.dock.widget.php';
+                        }
+                        include 'backend/widgets/conges_view.dock.widget.php';
 		?>
 	</ul>
 </div>
